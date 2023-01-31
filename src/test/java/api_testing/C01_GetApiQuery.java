79f5ab6108d0d602eec5707d80c0d5a0c227b587
omerboyraz <omer.boyraz@isuzu.com.tr>
2023-01-31 15:48:59 +0300

    herokuapp booking api test

---
+++ src/test/java/api_testing/C01_GetApiQuery.java
@@ -0,0 +1,31 @@
+package api_testing;
+
+import io.restassured.response.Response;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C01_GetApiQuery {
+
+    @Test
+    public void get01(){
+
+        String url = "https://restful-booker.herokuapp.com/booking/2505"; // id based book request
+
+        Response response = given().when().get(url);
+
+        response.prettyPrint();
+
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType("application/json; charset=utf-8").
+                header("Server","Cowboy").
+                statusLine("HTTP/1.1 200 OK");
+
+
+
+    }
+}
